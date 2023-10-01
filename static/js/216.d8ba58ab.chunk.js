ad50bddb5e056fc0f81dee1135809c3b45838945
Evgenij Prishchepov <prishchepovee@gmail.com>
2023-10-01 23:12:26 +0300

    Updates

---
+++ static/js/216.d8ba58ab.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[216],{216:function(e,n,t){t.r(n);var i=t(184);n.default=function(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)("h1",{children:"NotFound"}),(0,i.jsx)("div",{children:"Page NotFound"})]})}}}]);
+//# sourceMappingURL=216.d8ba58ab.chunk.js.map
